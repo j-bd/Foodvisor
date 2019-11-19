96e742f3df55b0c562dec9b60346da959b40a2a8
j-bd <recodata.out@gmail.com>
2019-11-19 10:17:29 +0100

    base structure

--- food_database/database.py
+++ food_database/database.py
@@ -6,3 +6,16 @@ Created on Tue Nov 19 09:22:48 2019
 @author: j-bd
 """
 
+class Database:
+    '''class making able to create items and linked with sub-items'''
+    def __init__(self):
+        '''create a new object with the following base structure'''
+
+    def add_nodes(self, list_tup):
+        '''create child node'''
+
+    def add_extract(self, dic):
+        '''linked image with node(s)'''
+
+    def get_extract_status(self, img):
+        '''give the status of an image'''
