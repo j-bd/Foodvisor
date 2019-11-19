6732b7a9f862938ae61f7fb4312fb4cfdde37953
j-bd <recodata.out@gmail.com>
2019-11-19 17:43:36 +0100

    add_nodes move into a graph

--- food_database/database.py
+++ food_database/database.py
@@ -6,6 +6,7 @@ Created on Tue Nov 19 09:22:48 2019
 @author: j-bd
 """
 import logging
+import copy
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
 
@@ -14,25 +15,34 @@ class Database:
 
     def __init__(self, root_node):
         '''create a new object with the following base structure'''
-        self.graph = [(root_node, None),]
-        self.im_extract = dict()
+        self.name = root_node
+        self.graph = {self.name : None}
+        self.im_extract = list()
 
     def add_nodes(self, list_tup):
-        '''create child node if parent node exist'''
+        '''edit graph with parents and childre nodes'''
         for child_id, parent_id in list_tup:
-            if parent_id in [node[0] for node in self.graph]:
-                self.graph.append((child_id, parent_id))
+            if parent_id == self.name:
+                self.graph[child_id] = list()
+            elif parent_id in self.graph.keys():
+                list_child = self.graph[parent_id]
+                list_child.append(child_id)
+                self.graph[parent_id] = list_child
             else:
                 logging.error(
-                    f" '{parent_id}' does not exist in current graph\n"\
+                    f" '{parent_id}' does not exist in current graph :\n"\
+                    f"{self.graph}"\
                     f"Please, add '{parent_id}' as a child id and its parent id before."
-                    )
+                )
 
     def add_extract(self, dic):
-        '''linked image with node(s)'''
-        self.im_extract = {
-            im_names : list_ids for im_names, list_ids in dic.items()
-            }
+        '''link image information with current graph'''
+#        self.im_extract = {
+#            im_names : list_ids for im_names, list_ids in dic.items()
+#        }
+        self.im_extract.append((dic, copy.deepcopy(self.graph)))
 
     def get_extract_status(self, img):
         '''give the status of an image'''
+        #Comparison of current self.graph and graph existing at 'add_extract' creation
+
