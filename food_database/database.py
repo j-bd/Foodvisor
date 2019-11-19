c6995c35a701ae0eb72ead6380e9ef8e7f050a15
j-bd <recodata.out@gmail.com>
2019-11-19 14:27:46 +0100

    elements names modification

--- food_database/database.py
+++ food_database/database.py
@@ -9,23 +9,25 @@ Created on Tue Nov 19 09:22:48 2019
 class Database:
     '''class making able to create items and linked with sub-items'''
 
-    def __init__(self, master_node):
+    def __init__(self, root_node):
         '''create a new object with the following base structure'''
-        self.graph = [(master_node, None),]
-        self.images = dict()
+        self.graph = [(root_node, None),]
+        self.im_extract = dict()
 
     def add_nodes(self, list_tup):
         '''create child node if parent node exist'''
-        for child_node, parent_node in list_tup:
-            if parent_node in [node[0] for node in self.graph]:
-                self.graph.append((child_node, parent_node))
+        for child_id, parent_id in list_tup:
+            if parent_id in [node[0] for node in self.graph]:
+                self.graph.append((child_id, parent_id))
             else:
                 print(
-                    f"error, {parent_node} does not exist in current graph\n"\
-                    f"Please, add {parent_node} as a child node and its parent.")
+                    f"error, {parent_id} does not exist in current graph\n"\
+                    f"Please, add {parent_id} as a child id and its parent id."
+                    )
 
     def add_extract(self, dic):
         '''linked image with node(s)'''
+
 
     def get_extract_status(self, img):
         '''give the status of an image'''
