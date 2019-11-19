3b6aafed5471c0a67983220f9e5bc6f6197e730c
j-bd <recodata.out@gmail.com>
2019-11-19 15:10:02 +0100

    Instance method add_extract

--- food_database/database.py
+++ food_database/database.py
@@ -5,6 +5,9 @@ Created on Tue Nov 19 09:22:48 2019
 
 @author: j-bd
 """
+import logging
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.ERROR)
 
 class Database:
     '''class making able to create items and linked with sub-items'''
@@ -20,14 +23,16 @@ class Database:
             if parent_id in [node[0] for node in self.graph]:
                 self.graph.append((child_id, parent_id))
             else:
-                print(
-                    f"error, {parent_id} does not exist in current graph\n"\
-                    f"Please, add {parent_id} as a child id and its parent id."
+                logging.error(
+                    f" '{parent_id}' does not exist in current graph\n"\
+                    f"Please, add '{parent_id}' as a child id and its parent id before."
                     )
 
     def add_extract(self, dic):
         '''linked image with node(s)'''
-
+        self.im_extract = {
+            im_names : list_ids for im_names, list_ids in dic.items()
+            }
 
     def get_extract_status(self, img):
         '''give the status of an image'''
